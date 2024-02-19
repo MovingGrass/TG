26b8e00a8a1bf277d6857ae4bf73c16313a3bf78
MovingGrass <cocobear.dmt@gmail.com>
2024-02-19 13:00:36 +0700

    lll

--- My project/Assets/Player_Movement.cs
+++ My project/Assets/Player_Movement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
-    public float RunSpeed = 40f;
+    public float walkSpeed = 40f;  // Default walk speed
+    public float runSpeedMultiplier = 2.5f;  // Multiplier for run speed
+    private float speed;  // Current speed
 
-    bool crouch = false; 
-
+    bool crouch = false;
     bool jump = false;
 
     float horizontalMove = 0f;
@@ -17,26 +19,47 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        // Set speed based on whether running or walking
+        speed = walkSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRunning", true);
+            speed *= runSpeedMultiplier;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        // Update animator parameters
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove) * speed);
 
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("Jumping", true);
         }
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
-        } else if (Input.GetButtonUp("Crouch"))
+        }
+        else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
         }
     }
 
-   
+    public void OnLanding()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        controller.Move(horizontalMove * speed * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
 }
